cfd4240d9fe5cc87fb3e3b697c449391ba93a264
vincenttommi <vincenttommi@gmail.com>
2023-08-16 21:05:07 +0300

    updating navigation bar

--- port/src/Components/Navbar.js
+++ port/src/Components/Navbar.js
@@ -4,11 +4,16 @@ const Navbar = () => {
   return (
    <div className='flex justify-between items-center px-4 w-full h-20 text-white fixed bg-black'>
       <div>
-
-     <h1 className='text-5xl'>Vincent</h1>
-
-
+     <h1 className='text-5xl font-signature ml-4'>Vincent</h1>
       </div>
+
+    <ul className="flex">
+        <li className='px-4   cursor-pointer'>Home</li>
+        <li className="">Home</li>
+       
+
+    </ul>
+
    </div>
   )
 }
